35144544729a10df125f1acf78e8955f1287f441
TwistyPassages <robert_n_walker@yahoo.ca>
2021-01-18 11:07:20 -0700

    2 files modified, 1 added

--- src/app/add-question/add-question.component.ts
+++ src/app/add-question/add-question.component.ts
@@ -1,67 +1,74 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { QuestionnaireMaintenanceService } from '../questionnaire-maintenance.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { QuestionnaireMaintenanceService } from "../questionnaire-maintenance.service";
 
 @Component({
-  selector: 'app-add-question',
-  templateUrl: './add-question.component.html',
-  styleUrls: ['./add-question.component.css']
+  selector: "app-add-question",
+  templateUrl: "./add-question.component.html",
+  styleUrls: ["./add-question.component.css"]
 })
 export class AddQuestionComponent implements OnInit {
-
   addQuestionForm = this.formBuilder.group({
-    code: ['', Validators.required],
-    internal: '',
-    category: ['', Validators.required],
-    description: ['', Validators.required],
-    answerStyle: ''
+    code: ["", Validators.required],
+    internal: "",
+    category: ["", Validators.required],
+    description: ["", Validators.required],
+    answerStyle: ["", Validators.required]
   });
 
-  isSubmitted;
-  message;
 
   constructor(
-      private formBuilder: FormBuilder,
-      private questionnaireService: QuestionnaireMaintenanceService
-  ) { }
+    private formBuilder: FormBuilder,
+    private questionnaireService: QuestionnaireMaintenanceService
+  ) {}
 
+  ngOnInit() {}
 
-
-
-  ngOnInit() {
-  }
-
-
-  onSubmit(): void {  
-    console.log('onSubmit()');
+  onSubmit(): void {
+    console.log("onSubmit()");
     var isInternal = false;
-    if (this.addQuestionForm.get('internal').value) {
+    if (this.addQuestionForm.get("internal").value) {
       isInternal = true;
     }
 
-    var code = this.addQuestionForm.get('code').value;
-    var code = this.addQuestionForm.get('description').value;
-    var code = this.addQuestionForm.get('answerStyle').value;
+    var code = this.addQuestionForm.get("code").value;
+    var category = this.addQuestionForm.get("category").value;
+    var description = this.addQuestionForm.get("description").value;
+    var answerStyle = this.addQuestionForm.get("answerStyle").value;
+    console.log('answer style: ' + answerStyle);
 
+    var answers = [];
+    if (answerStyle === 'YesNo') {
+      console.log('doing it');
+      answers.push({
+        code: "true",
+        display: "Yes"
+      });
+      answers.push({
+        code: "false",
+        display: "No"
+      });
+    }
+
+console.log(JSON.stringify(answers));
 
 
     var currentTime = new Date();
     var newQuestion = {
-        id: currentTime.getTime(),
-        category: "Age",
-        code: this.addQuestionForm.get('code').value,
-        description : this.addQuestionForm.get('description').value,
-        internal: isInternal,
-        answerStyle: "yesNo"
+      id: currentTime.getTime(),
+      category: category,
+      code: code,
+      description: description,
+      internal: isInternal,
+      answerStyle: answerStyle,
+      answerOptions: answers
     };
     this.questionnaireService.addQuestion(newQuestion);
 
+
     // Process checkout data here
-    console.warn('Your order has been submitted', this.addQuestionForm.value);
+    console.warn("Your order has been submitted", this.addQuestionForm.value);
     this.addQuestionForm.reset();
 
-    this.isSubmitted = true;
-    this.message = 'The new question for code ' + this.addQuestionForm.get('code').value + ' has been added';
   }
-
 }
